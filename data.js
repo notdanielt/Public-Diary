4a14040f11ef420c2b2b0e735f41d8cf9d5187bc
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-08-24 19:41:55 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+//NOTE: This list is ordered inversely by week. For example, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21] would become [15,16,17,18,19,20,21,8,9,10,11,12,13,14,1,2,3,4,5,6,7].
+data = [
+    [[2025, 8, 24], 6, "Awake: 12:00 AM to ???, no shower, no exercise", "The first day!\nWorked on Omnincremental quite a lot, but more importantly, this page exists now! This is because this is the week that high school begins for me.\nOther than that, though, I didn't get too many things done. Still overall a good day."],
+    //[[2025, 8, 25], 5, ""],
+    //[[2025, 8, 26], 5, ""],
+];
