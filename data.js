965d850c3568fef3df9d9adec926306302dd3df5
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-08-25 00:58:08 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 //NOTE: This list is ordered inversely by week. For example, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21] would become [15,16,17,18,19,20,21,8,9,10,11,12,13,14,1,2,3,4,5,6,7].
 data = [
-    [[2025, 8, 24], 6, "Awake: 12:00 AM to ???, no shower, no exercise", "The first day!\nWorked on Omnincremental quite a lot, improved the home page, but most importantly, this page exists now! This is because this is the week that high school begins for me.\nOther than that, I finished up my annoying Math summer homework (finally), and am aiming to complete the LA stuff tomorrow. Overall, a pretty good day; I don't know whether to put this in 6/10 or 7/10 but I'm leaning towards 6/10 due to today being pretty uneventful (of course, it was still good, though)."],
+    [[2025, 8, 24], 6, "Awake: 12:00 AM to 1:00 AM, no shower, no exercise", "The first day!\nWorked on Omnincremental quite a lot, improved the home page, but most importantly, this page exists now! This is because this is the week that high school begins for me.\nOther than that, I finished up my annoying Math summer homework (finally), and am aiming to complete the LA stuff tomorrow. Overall, a pretty good day; I don't know whether to put this in 6/10 or 7/10 but I'm leaning towards 6/10 due to today being pretty uneventful (of course, it was still good, though)."],
     //[[2025, 8, 25], 5, ""],
     //[[2025, 8, 26], 5, ""],
 ];
