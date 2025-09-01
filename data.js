b4e8a7dae6f0d6c59f605033f97376e9b9e0779c
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-09-01 09:57:11 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 //NOTE: This list is ordered inversely by week. For example, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21] would become [15,16,17,18,19,20,21,8,9,10,11,12,13,14,1,2,3,4,5,6,7].
 data = [
     [[2025, 8, 31], 5, "Awake: 10:00 AM to 10:20 PM, no shower, no exercise", "I'm not gonna sugarcoat it: nothing of value actually happened today. More stuff will happen tomorrow for sure, but for now, this will be the shortest description."],
-    //[[2025, 9, 1], 5, "", ""],
-    //[[2025, 9, 2], -1, "", ""],
-    //[[2025, 9, 3], -1, "", ""],
-    //[[2025, 9, 4], -1, "", ""],
-    //[[2025, 9, 5], -1, "", ""],
-    //[[2025, 9, 6], -1, "", ""],
+    [[2025, 9, 1], -1, "", ""],
+    [[2025, 9, 2], -1, "", ""],
+    [[2025, 9, 3], -1, "", ""],
+    [[2025, 9, 4], -1, "", ""],
+    [[2025, 9, 5], -1, "", ""],
+    [[2025, 9, 6], -1, "", ""],
     [[2025, 8, 24], 6, "Awake: 12:00 AM to 1:20 AM, no shower, no exercise", "The first day!\nWorked on Omnincremental quite a lot, improved the home page, but most importantly, this page exists now! This is because this is the week that high school begins for me.\nOther than that, I finished up my annoying Math summer homework (finally), and am aiming to complete the LA stuff tomorrow. Overall, a pretty good day; I don't know whether to put this in 6/10 or 7/10 but I'm leaning towards 6/10 due to today being pretty uneventful (of course, it was still good, though)."],
     [[2025, 8, 25], 5, "Awake: 10:00 AM to 4:00 PM + 8:00 PM to 1:00 AM, no shower, no exercise", "The second day!\nI was about as productive in homework as I was yesterday, completing nearly all of the LA homework I had promised to complete. Other than that, today's 6/10 productivity is cancelled by the 4/10 rest of the day, with me doing nothing, as well as taking a 4 hour nap in the middle. Tomorrow is the final day before high school begins, so let's hope it's a good one! (actually it will probably just be another 5/10)"],
     [[2025, 8, 26], 5, "Awake: 1:30 PM to 10:00 PM, no shower, no exercise", "The thir-ok I'll stop now. But for real, today is the last day before high school, and it's kinda crazy... Also, I guess I was right about today being a 5/10 yesterday. Anyway, I fnished up the remaining portion of the LA homework just in time, and the rest of the day was basically nothing, so yeah. High school will make things more interesting, though..."],
