97bfe60300da6b5fd5271fa8180bc26bc36e0d82
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-11-10 22:09:53 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 //NOTE: This list is ordered inversely by week. For example, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21] would become [15,16,17,18,19,20,21,8,9,10,11,12,13,14,1,2,3,4,5,6,7].
 data = [
     [[2025, 11, 9], 6, "10:00 AM to 9:40 PM", "My Biology grade is back to a B- (which isn't great for my standards, but it's acceptable)!! I could've done more today, but that didn't affect today too much, especially considering there isn't much to do. I wonder how tomorrow will go..."],
-    [[2025, 11, 10], -1, "", ""],
+    [[2025, 11, 10], 3, "6:00 AM to 10:10 PM", "4 bad periods in a row, and on top of that, I couldn't get any homework done. Good thing there's no school tomorrow. (This is very close to being a 4, don't interpret this too badly)"],
     [[2025, 11, 11], -1, "", ""],
     [[2025, 11, 12], -1, "", ""],
     [[2025, 11, 13], -1, "", ""],
