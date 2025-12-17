d56acf595062960decf67c677120c4e784db7e4d
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-12-16 22:52:35 -0500

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 data = [
     [[2025, 12, 14], 4, "9:00 AM to 3:20 PM + 7:40 PM to 11:50 PM", "Today just felt... wrong. I couldn't bring myself to do anything, even when I was feeling bored of idling. Even with productivity being right in front of me, and my distractions eliminated, I somehow found a way to fumble the bag and take a 4 hour nap that ruined the pace. How did that happen?"],
     [[2025, 12, 15], 6, "6:00 AM to 9:50 PM", "I was not expecting yesterday's suboptimal sleep schedule to result in today being not bad! I was more motivated today than usual, although apparently not enough to complete any homework after school. Hey, it's better than before."],
-    [[2025, 12, 16], -1, "", ""],
+    [[2025, 12, 16], 5, "6:00 AM to 11:00 PM", "Basically the same as yesterday, minus the sleep schedule. Motivated, yet not motivated enough."],
     [[2025, 12, 17], -1, "", ""],
     [[2025, 12, 18], -1, "", ""],
     [[2025, 12, 19], -1, "", ""],
