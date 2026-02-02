eaae5c4aede0ff22be8f4a89b745000e1ebc2adc
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2026-02-01 22:03:36 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
 //NOTE: This list is ordered inversely by week, but normally within each week. This is to show the most recent weeks at the top, so you don't have to scroll down to see recent activity.
 data = [
+    [[2026, 2, 1], 6, "10:20 AM to 10:10 PM", "Welcome to February! I was actually really productive for the first half of the day, but foiled in the second half. This is only the first day though, and I still did enough today to be satisfactory."],
+    [[2026, 2, 2], -1, "", ""],
+    [[2026, 2, 3], -1, "", ""],
+    [[2026, 2, 4], -1, "", ""],
+    [[2026, 2, 5], -1, "", ""],
+    [[2026, 2, 6], -1, "", ""],
+    [[2026, 2, 7], -1, "", ""],
     [[2026, 1, 25], 5, "10:40 AM to 12:00 AM, showered", "I'm just now starting to realize how uneventful most of my life is. I'm motivated to do stuff tomorrow, but we'll see how long that lasts."],
     [[2026, 1, 26], 5, "i forgor to 10:10 PM", "Well, I didn't do as much as I'd hoped, but I probably still did better than I expected. I have some interesting plans for February in the works, and I'll try to warm up for them for these last few days."],
     [[2026, 1, 27], 4, "6:00 AM to 10:00 PM, showered", "Yo I actually need to start doing things, this is getting out of hand. I'm not about to relive the same experience I had a year ago (but you probably don't know about that, it was before I made this diary)."],
