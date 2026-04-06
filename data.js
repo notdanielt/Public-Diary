ea21b7bddd4d13eb1432da593addfdec2417f643
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2026-04-05 21:52:43 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
 //NOTE: This list is ordered inversely by week, but normally within each week. This is to show the most recent weeks at the top, so you don't have to scroll down to see recent activity.
 data = [
+    [[2026, 4, 5], 5, "8:00 AM to 10:00 PM, showered", "I'm not sure what to think about today. Yes, I did get something done, but I still wanted to do more. I guess that's all I can really ask for. Let's see what this new week brings upon me."],
+    [[2026, 4, 6], -1, "", ""],
+    [[2026, 4, 7], -1, "", ""],
+    [[2026, 4, 8], -1, "", ""],
+    [[2026, 4, 9], -1, "", ""],
+    [[2026, 4, 10], -1, "", ""],
+    [[2026, 4, 11], -1, "", ""],
     [[2026, 3, 29], 5, "9:50 AM to 10:00 PM, showered", "I was planning on being productive today, but it only partially went through. A lot of time was wasted and I started to feel sick at around 6 PM. Not sure how tomorrow will go."],
     [[2026, 3, 30], 5, "i forgor to 10:10 PM", "Didn't go to school due to the sickness I mentioned yesterday. I also didn't get anything done today despite getting better in the afternoon."],
     [[2026, 3, 31], 4, "6:00 AM to 9:50 PM", "Rounding off March, I'm not sure what to think. For now, I'll probably just keep on trying to improve like I have been."],
